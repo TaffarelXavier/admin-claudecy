8b446e57b5a260c1cccbd8391f8dca1608ef47e5
Taffarel Xavier <taffarel_deus@hotmail.com>
2020-02-14 12:31:22 -0300

    first commit

---
+++ precache-manifest.7cd9a58833abe184cf9f4c7752998911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94734defc777a70f29e763c382ab0ff6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06debc41f144a0d5bafa",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dc37dd2c67697f56d9d5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "06debc41f144a0d5bafa",
+    "url": "/static/js/2.d76fdee8.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.d76fdee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc37dd2c67697f56d9d5",
+    "url": "/static/js/main.2506f5e5.chunk.js"
+  },
+  {
+    "revision": "4be3212a6299ad925ceb",
+    "url": "/static/js/runtime-main.fe3b34d3.js"
+  }
+]);
